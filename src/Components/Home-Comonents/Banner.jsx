6c80468e7d3885ac3bf2892c5e3b4cf5759ac915
Bushra01-Dhaka <nothingg192@gmail.com>
2025-12-09 20:54:51 +0600

    Created Banner component , make it responsive and also used GSAP for animation

--- src/Components/Home-Comonents/Banner.jsx
+++ src/Components/Home-Comonents/Banner.jsx
@@ -1,11 +1,63 @@
-
+import tinyMan from "../../assets/tiny-deliveryman.png";
+import riderImg from "../../assets/riderImg.png";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useRef } from "react";
+gsap.registerPlugin(ScrollTrigger);
 
 const Banner = () => {
-  return (
-    <div>
-        <h2 className="text-3xl text-primary">Fastest Delivary Parcelio</h2>
-    </div>
-  )
-}
+  const bannerRef = useRef(null);
 
-export default Banner
+  useEffect(() => {
+     const t1 = gsap.timeline();
+
+     t1.to("#rider", {
+      right:"8%",
+      duration:3,
+      delay:1
+     },'one')
+      t1.to("#tinyRider", {
+      left:"5%",
+      duration:4,
+      delay:1
+     },'one')
+
+  }, [])
+
+  return (
+    <div ref={bannerRef} className="py-10 flex justify-center items-center min-h-[100vh] px-8 lg:px-6">
+      <div className="flex flex-col lg:flex-row justify-between items-center gap-10 text-secondary">
+        {/* left */}
+        <div className="flex-1 lg:w-[630px] py-4">
+          <img id="tinyRider" className="relative left-[-150px]" src={tinyMan} alt="" />
+          <h2 className="text-secondary text-4xl lg:text-6xl font-extrabold">
+            We Make Sure Your{" "}
+            <span className="text-primary">Parcel Arrives</span> On Time – No
+            Fuss.
+          </h2>
+          <p className="py-4">
+            Enjoy fast, reliable parcel delivery with real-time tracking and
+            zero hassle. From personal packages to business shipments — we
+            deliver on time, every time.
+          </p>
+          <div className="py-6 font-semibold">
+            <a className="btn btn-secondary btn-outline text-secondary hover:text-primary rounded-2xl mr-2 ">
+              Sign In
+            </a>
+            <a className="btn btn-primary text-secondary rounded-2xl ">
+              Be a Rider
+            </a>
+          </div>
+        </div>
+        {/* right */}
+        <div>
+           <img id="rider" className="relative right-[-150px] p-4" src={riderImg} alt="" />
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { router } from './Routes/router';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='md:max-w-screen-1xl mx-auto font-urbanist  bg-[#016A70]'>
+    <div className='md:max-w-screen-1xl mx-auto font-urbanist  bg-[#016A70] lg:pt-4'>
        <RouterProvider router={router} />
     </div>
   </StrictMode>,
